b2ebcdfbe0b9a68ea193b0779acc6e0d637082a9
William Zimmerman <will.zimmerman@gmail.com>
2025-05-31 09:27:47 -0400

    Adding haskell-tools; keeping disabled for now

---
+++ lua/plugins/haskell-tools.lua
@@ -0,0 +1,10 @@
+-- haskell-tools.nvim
+-- https://github.com/mrcjkb/haskell-tools.nvim
+
+return {
+  enabled = false,
+  "mrcjkb/haskell-tools.nvim",
+  version = "^6", -- Recommended
+  lazy = false, -- This plugin is already lazy
+}
+
