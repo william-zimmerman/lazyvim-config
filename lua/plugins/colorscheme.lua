04b589638c14b3bf73f621afb823e3b7fd27bb1a
William Zimmerman <will.zimmerman@gmail.com>
2025-05-31 09:20:36 -0400

    Setting colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add themes
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave",
+    },
+  }
+}
